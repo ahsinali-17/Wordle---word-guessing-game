06184fb6605af1b8c40eceb446b1c9b88ae19917
ahsinali-17 <aajoker17@gmail.com>
2025-03-30 01:39:52 +0500

    completed

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -4,14 +4,14 @@ const Info = () => {
   return (
     <div className="bg-gray-300 h-full w-full p-4 text-xs font-semibold">
         <ul className="flex flex-col gap-2">
-            <li className="flex gap-2 items-center"> <img src="/src/assets/dot.svg" alt="dot"/> You have 6 chances to guess the word.</li>
-            <li className="flex gap-2 items-center"> <img src="/src/assets/dot.svg" alt="dot"/> Each guess must be a valid 5-letter word.</li>
-            <li className="flex gap-2 items-center"> <img src="/src/assets/dot.svg" alt="dot"/> The color of the tiles will change to show how close your guess was to the word.</li>
-            <li className="flex gap-2 items-center"> <img src="/src/assets/dot.svg" alt="dot"/><p><span className='text-green-500'>Green</span> tile means correct position.</p> </li>
-            <li className="flex gap-2 items-center"> <img src="/src/assets/dot.svg" alt="dot"/><p><span className='text-yellow-500'>yellow</span> tile means correct letter but wrong position.</p> </li>
-            <li className="flex gap-2 items-center"> <img src="/src/assets/dot.svg" alt="dot"/> <p><span className='text-gray-500'>Gray</span> means the letter is not in the word.</p></li>
-            <li className="flex gap-2 items-center"> <img src="/src/assets/dot.svg" alt="dot"/> You can use the hint button to get a hint till 4 wrong guesses.</li>
-            <li className="flex gap-2 items-center"> <img src="/src/assets/dot.svg" alt="dot"/> But there is a catch, every time you get a hint, you will lose a chance.</li>
+            <li className="flex gap-2 items-center"> <img src="./src/assets/dot.svg" alt="dot"/> You have 6 chances to guess the word.</li>
+            <li className="flex gap-2 items-center"> <img src="./src/assets/dot.svg" alt="dot"/> Each guess must be a valid 5-letter word.</li>
+            <li className="flex gap-2 items-center"> <img src="./src/assets/dot.svg" alt="dot"/> The color of the tiles will change to show how close your guess was to the word.</li>
+            <li className="flex gap-2 items-center"> <img src="./src/assets/dot.svg" alt="dot"/><p><span className='text-green-500'>Green</span> tile means correct position.</p> </li>
+            <li className="flex gap-2 items-center"> <img src="./src/assets/dot.svg" alt="dot"/><p><span className='text-yellow-500'>yellow</span> tile means correct letter but wrong position.</p> </li>
+            <li className="flex gap-2 items-center"> <img src="./src/assets/dot.svg" alt="dot"/> <p><span className='text-gray-500'>Gray</span> means the letter is not in the word.</p></li>
+            <li className="flex gap-2 items-center"> <img src="./src/assets/dot.svg" alt="dot"/> You can use the hint button to get a hint till 4 wrong guesses.</li>
+            <li className="flex gap-2 items-center"> <img src="./src/assets/dot.svg" alt="dot"/> But there is a catch, every time you get a hint, you will lose a chance.</li>
         </ul>
     </div>
   )
